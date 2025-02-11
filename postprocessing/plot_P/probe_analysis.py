200889574a471f7a3664def686038850875c1cdb
abhirup-roy <abhiruproy2002@gmail.com>
2025-02-11 19:28:55 +0000

    Updated python script

--- postprocessing/plot_P/probe_analysis.py
+++ postprocessing/plot_P/probe_analysis.py
@@ -1,61 +1,19 @@
 #!/usr/bin/env python3
 import os
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import plotly.express as px
 
-"""
-probe_path = '../../CFD/postProcessing/probes/0/p'
-
-# Check if correct path is provided
-if not os.path.exists(probe_path):
-    raise Exception(f"Path {probe_path} does not exist")
-
-headers = ["Probe Time", "Probe 0", "Probe 1", "Probe 2", "Probe 3", "Probe 4"]
-
-# Extract pressure at probes
-probe_df = pd.read_csv(
-    probe_path, 
-    delim_whitespace=True, 
-    comment='#', 
-    names=headers, 
-    header=None,
-).set_index("Probe Time")
-
-probe_df.to_csv("probe_pressure.csv")
-
-#Matplotlib plotting
-
-plt.figure(figsize=[20,10])
-
-probe_df.plot(xlabel="Time (s)", ylabel="Pressure (Pa)", title="Pressure at Probes")
-plt.savefig("probe_pressure.png")
-"""
-
-# pd.options.plotting.backend = "plotly"
-# times_lst = [0,0.5, 0.6, 1.6, 1.7, 2.7, 2.8, 3.8, 3.9, 4.9, 5, 6,
-#              6.1, 7.1, 7.2, 8.2, 8.3, 9.3, 9.4, 10.4, 10.5, 11.5,
-#              12.5, 12.6, 13.6, 13.7]
-
-# prob_px = px.line(probe_df, title="Pressure at Probes")
-# prob_px.update_xaxes(title_text="Time (s)")
-# prob_px.update_yaxes(title_text="Pressure (Pa)")
-# prob_px.update_layout(hovermode="x unified")
-# prob_px.write_html("probe_pressure.html")
-
-
-# probe_px = probe_df.plot(title="Pressure at Probes", template="simple_white",
-#               labels=dict(index="Time (s)", value="Pressure", variable="Probe"))
-
-# for time in times_lst:
-#     probe_px.add_vline(x=time, line_width=3, line_dash="dash", line_color="green")
-
-
-# probe_px.write_html("probe_pressure.html")
-
-class Probe():
+class ProbeAnalysis():
 
     def __init__(self, probe_path, nprobes, probes_text_path, dump2csv=True):
+        """
+        probe_path: Path to the probe file
+        nprobes: Number of probes
+        probes_text_path: Path to the probes.txt file
+        dump2csv: Save the probe data to a csv file
+        """
         self.probe_path = probe_path
         self.nprobes = nprobes
         self.dump2csv = dump2csv
@@ -106,6 +64,8 @@ class Probe():
                 line_splt = line.replace("(", "").replace(")", "").split()
                 self.t.append(float(line_splt[0]))
                 self.v_z.append(float(line_splt[-1]))
+            print("Selected times: ", self.t)
+            print("Corresponding vel: ", self.v_z)
 
     def _calc_vel(self):
         bounds = []
@@ -114,36 +74,51 @@ class Probe():
         self._read_probetxt()
         
         for i in range(len(self.t)-1):
-            if self.t[i] == self.t[i+1]:
+            if self.v_z[i] == self.v_z[i+1]:
                 bounds.append([self.t[i], self.t[i+1]])
                 vel.append(self.v_z[i])
             else:
                 pass
         
-        def _vel_mask(df, bounds, vel):
-            for i in range(len(bounds)):
-                if df["Probe Time"] < bounds[i][0] and df["Probe Time"] > bounds[i][1]:
-                    df["V_z"] = vel[i]
-                else: pass
+        # def _vel_mask(df, bounds, vel):
+        #     for i in range(len(bounds)):
+        #         if df["Probe Time"] < bounds[i][0] and df["Probe Time"] > bounds[i][1]:
+        #             df["V_z"] = vel[i]
+        #         else: pass
+        # self.probe_df = self.probe_df.apply(_vel_mask, args=(bounds, vel), axis=1)
+
+        lb = [b[0] for b in bounds]
+        ub = [b[1] for b in bounds]
+        try:
+            vz_arr = np.zeros_like(self.probe_df.index.to_numpy())
         
-        self.probe_df = self.probe_df.apply(_vel_mask, args=(bounds, vel), axis=1)
+            for i in range(len(bounds)):
+                mask = (self.probe_df.index.to_numpy() > lb[i]) & (self.probe_df.index.to_numpy() < ub[i])
+                vz_arr[mask] = vel[i]
+        except:
+            print(self.probe_df)
+
+        self.probe_df["V_z"] = vz_arr
+
 
     def plot_pressure_v(self):
         self._calc_vel()
-        
-        print(self.probe_df)
 
-        plt.plot(x=self.probe_df["V_z"], xlabel="Time (s)", ylabel="Velocity (m/s)")
+        print(self.probe_df)
+        plt.figure(figsize=[30,20])
+        for i in range(self.nprobes):
+            plt.scatter(self.probe_df["V_z"], self.probe_df[f"Probe {i}"], label=f"Probe {i}",)
+            
+        plt.legend()
         plt.savefig("pressure_vel_plot.png")
     
 
 
 if __name__ == "__main__":
     probe_path = '../../CFD/postProcessing/probes/0/p'
-    probes_text_path = 'probes.txt'
+    probes_text_path = 'probes_txt'
 
-    probe_cfdem = Probe(probe_path=probe_path, nprobes=5, probes_text_path=probes_text_path, dump2csv=True)
-    # print(probe_cfdem.probe_df)
+    probe_cfdem = ProbeAnalysis(probe_path=probe_path, nprobes=5, probes_text_path=probes_text_path, dump2csv=True)
     probe_cfdem.plot_probe(plot_backend="plotly")
     probe_cfdem.plot_probe(plot_backend="matplotlib")
     probe_cfdem.plot_pressure_v()
