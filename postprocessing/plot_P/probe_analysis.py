61b49bf6303614f650593ca9a982ce5ddf35d171
abhirup-roy <abhiruproy2002@gmail.com>
2025-02-14 00:38:33 +0000

    Adding better commenting

--- postprocessing/plot_P/probe_analysis.py
+++ postprocessing/plot_P/probe_analysis.py
@@ -13,7 +13,9 @@ class ProbeAnalysis():
         nprobes: Number of probes
         velcfg_path: Path to the vel_cfg file
         dump2csv: Save the probe data to a csv file
+        plots_dir: Directory to save the plots
         """
+        # Check if the paths exist
         if not os.path.exists(pressure_path):
             raise Exception(f"Pressure data at {pressure_path} does not exist")
         if not os.path.exists(velcfg_path):
@@ -33,7 +35,7 @@ class ProbeAnalysis():
         """
         Convert the probe data to a pandas dataframe, with data indexed by time. Dump if specified
         """
-
+        # Make df from the probe data
         headers = ["Probe Time"]
         for i in range(self.nprobes):
             headers.append(f"Probe {i}")
@@ -46,6 +48,7 @@ class ProbeAnalysis():
             header=None,
         ).set_index("Probe Time")
 
+        # Dump to csv if specified
         if self.dump2csv:
             self.probe_df.to_csv("probe_pressure.csv")
 
@@ -53,9 +56,9 @@ class ProbeAnalysis():
         """
         Plot the pressure data at the probes ported to matplotlib or plotly
 
-        plot_backend: "matplotlib" or "plotly"
+        plot_backend: "matplotlib" for png or "plotly" for interactive html
         """
-
+        # Plotly backend
         pd.options.plotting.backend = plot_backend
 
         if plot_backend == "plotly":
@@ -74,7 +77,8 @@ class ProbeAnalysis():
         """
         with open(self.velcfg_path, "r") as f:
             probe_text = f.read().splitlines(False)
-
+            
+            # read plot times and corresponding fluid velocities
             self.t = []
             self.v_z = []
 
@@ -89,11 +93,12 @@ class ProbeAnalysis():
         """
         Map the velocity to pressure
         """
+        # Initialise bounds and velocity
         bounds = []
         vel =[]
         
         self._read_probetxt()
-        
+        # Find the bounds for velocity
         for i in range(len(self.t)-1):
             if self.v_z[i] == self.v_z[i+1]:
                 bounds.append([self.t[i], self.t[i+1]])
@@ -104,11 +109,13 @@ class ProbeAnalysis():
 
             else:
                 pass
-
+        
+        # Lower and upper time bounds for each velocity 
         lb = [b[0] for b in bounds]
         ub = [b[1] for b in bounds]
         vz_arr = np.zeros_like(self.probe_df.index.to_numpy())
-    
+
+        # Map the velocity to the pressure data
         for i in range(len(bounds)):
             mask = (self.probe_df.index.to_numpy() > lb[i]) & (self.probe_df.index.to_numpy() < ub[i])
             vz_arr[mask] = vel[i]
@@ -120,15 +127,18 @@ class ProbeAnalysis():
         self.probe_df["V_z"] = vz_arr
 
 
-        def map_direction(x):
+        def _map_direction(x):
+            """
+            Simple helper function to map the direction of the velocity. i.e whether it is increasing, decreasing or at max
+            """
             if x < max_vel_t1:
                 return "up"
             elif x >= max_vel_t1 and x <= max_vel_t2:
                 return "max"
             else:
                 return "down"
-
-        self.probe_df["direction"] = self.probe_df.index.to_series().apply(map_direction)
+        # Add direction to the dataframe
+        self.probe_df["direction"] = self.probe_df.index.to_series().apply(_map_direction)
 
         
 
@@ -139,14 +149,15 @@ class ProbeAnalysis():
         """
         self._calc_vel()
 
-        # print(self.probe_df)
+        # Create the plot
         plt.figure(figsize=[20,10])
         
+        # Use groupby to get the mean pressure at each velocity for increasing, decreasing and max velocity
         vel_plot_df = self.probe_df.groupby(["direction", "V_z"]).mean()
+        # Set the pressure at zero velocity to zero
         vel_plot_df.loc[vel_plot_df.index.get_level_values('V_z') == 0, :] = 0
-
-        pd.options.plotting.backend = "matplotlib"
-         
+        
+        # Sort the data for plotting
         vel_up = vel_plot_df[
             vel_plot_df.index.get_level_values(level='direction').isin(["up", "max"])
         ].reset_index('direction', drop=True
@@ -161,8 +172,8 @@ class ProbeAnalysis():
             plt.plot(vel_up.index, vel_up[self.probe_df.columns[i]], label=self.probe_df.columns[i], color=f'C{i}', marker='o')
             plt.plot(vel_down.index, vel_down[self.probe_df.columns[i]], label=self.probe_df.columns[i], color=f'C{i}', marker='o', linestyle='dashed')
 
-        plt.xlabel(r"Velocity (m s^{-1})")
-        plt.ylabel(r"Pressure (Pa)")
+        plt.xlabel("Velocity (m/s)")
+        plt.ylabel("Pressure (Pa)")
 
         plt.savefig(self.plots_dir + "pressure_vel_plot.png")
     
