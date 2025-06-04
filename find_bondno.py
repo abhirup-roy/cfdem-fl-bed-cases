648e46a5a80461ed6f99a268e0f68dccc3245fa2
abhirup-roy <abhiruproy2002@gmail.com>
2025-06-04 22:07:58 +0100

    Better variable handling + JSON saving

--- find_bondno.py
+++ find_bondno.py
@@ -4,25 +4,29 @@
 """
 Calculates bond number for the simulation data using different models
 """
-
+import json
 from prepost.model_analysis import ModelAnalysis
 
-pressure_path = 'CFD/postProcessing/cuttingPlane/'
-velcfg_path = 'prepost/velcfg.txt'
+if __name__ == "__main__":
+        
+    pressure_path = 'CFD/postProcessing/cuttingPlane/'
+    velcfg_path = 'prepost/velcfg.txt'
 
-model = ModelAnalysis(
-    pressure_path='CFD/postProcessing/cuttingPlane/',
-    nprobes=5,
-    velcfg_path='postprocessing/plot_P/velcfg.txt',
-    dump2csv=False,
-    plots_dir='plots/'
-)
+    model = ModelAnalysis(
+        pressure_path=pressure_path,
+        nprobes=5,
+        velcfg_path=velcfg_path,
+        dump2csv=False,
+        plots_dir='plots/'
+    )
 
-model.define_params(
-    diameter=150e-6,
-    rho_p=2700,
-    bed_mass=0.0058,
-    cg_factor=2.44
-)
+    model.define_params(
+        diameter=150e-6,
+        rho_p=2700,
+        bed_mass=0.0058,
+        cg_factor=2.44
+    )
 
-print(model.model_summary())
+    print(model.model_summary())
+    with open('pyoutputs/model_summary.json', 'w') as f:
+        json.dump(model.model_summary(), f, indent=4)
